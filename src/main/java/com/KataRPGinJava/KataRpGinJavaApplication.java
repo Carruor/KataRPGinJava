a96247bbb25fd0eee0cb2b360be4d22b2a3b0b1b
Carruor <fotoes10@gmail.com>
2022-03-06 16:27:13 +0100

    initial commit

---
+++ src/main/java/com/KataRPGinJava/KataRpGinJavaApplication.java
@@ -0,0 +1,13 @@
+package com.KataRPGinJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataRpGinJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataRpGinJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KataRPGinJava/KataRpGinJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KataRPGinJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataRpGinJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
